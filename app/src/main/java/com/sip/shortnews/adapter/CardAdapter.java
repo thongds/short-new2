539bed7b926c8bd3a823393ebe4bd0888ccfd95b
thong <thongds@gmail.com>
2016-08-27 18:24:10 +0700

    fix cardview for androi L

--- app/src/main/java/com/sip/shortnews/adapter/CardAdapter.java
+++ app/src/main/java/com/sip/shortnews/adapter/CardAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.sip.shortnews.R;
+import com.sip.shortnews.Utilies.UtiliFunction;
 
 import java.util.ArrayList;
 import java.util.List;
