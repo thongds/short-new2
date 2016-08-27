667de1ce3304ddaddbbcb6c168303ea37c7d8310
thong <thongds@gmail.com>
2016-08-27 21:37:45 +0700

    add close event viewpager

--- app/src/main/java/com/sip/shortnews/fragment/DetailViewPageFragment.java
+++ app/src/main/java/com/sip/shortnews/fragment/DetailViewPageFragment.java
@@ -3,10 +3,16 @@ package com.sip.shortnews.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.sip.shortnews.MainActivity;
 import com.sip.shortnews.R;
 import com.sip.shortnews.adapter.CardAdapter;
 
@@ -20,8 +26,25 @@ public class DetailViewPageFragment extends PFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.detail_view_page_layout,container,false);
         ViewPager viewPager = (ViewPager)v.findViewById(R.id.viewPager);
+
+        final MainActivity mainActivity = (MainActivity) getActivity();
+        LinearLayout linearLayout1 = (LinearLayout)v.findViewById(R.id.ll_close1);
+        LinearLayout linearLayout2 = (LinearLayout)v.findViewById(R.id.ll_close2);
         CardAdapter cardAdapter = new CardAdapter(getContext());
         viewPager.setAdapter(cardAdapter);
+        linearLayout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainActivity.onBackPressed();
+            }
+        });
+        linearLayout2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainActivity.onBackPressed();
+            }
+        });
         return v;
     }
+
 }
